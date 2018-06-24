3fced12b7c017aef397950d1157e5b3176f56948
Vibhor Chaturvedi <vibhorchaturvedi1997@gmail.com>
2018-06-24 13:05:52 +0530

    add more package dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/index.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Store
+import store from './store/index';
+
+// Views
+import { App, Home } from './views/Components/index';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App>
+        <Route exact path='/' component={Home}/>
+      </App>
+    </Router>
+  </Provider>
+  , document.getElementById('root'));
+
+registerServiceWorker(); // eslint-disable-line no-undef
