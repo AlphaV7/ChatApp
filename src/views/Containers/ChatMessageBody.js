4dea92b470eb57d3cb2558a78addfbb70dd8770b
Vibhor Chaturvedi <vibhorchaturvedi1997@gmail.com>
2018-06-26 00:51:50 +0530

    add auto-scroll to bottom within chat body

--- src/views/Containers/ChatMessageBody.js
+++ src/views/Containers/ChatMessageBody.js
@@ -8,12 +8,31 @@ export class ChatMessageBody extends React.Component {
     super(props);
   }
 
+  scrollToBottom = () => {
+    this.messagesEnd.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  componentDidMount() {
+    const { messageListElements } = this.props;
+
+    messageListElements.length > 5 && this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    const { messageListElements } = this.props;
+
+    messageListElements.length > 5 && this.scrollToBottom();
+  }
+
   render(){
     const { messageListElements } = this.props;
 
     return (
       <div className="chat-body">
         <MessageList messageListElements={messageListElements}/>
+        <div
+          className="scroll-to-bottom"
+          ref={(el) => { this.messagesEnd = el; }}></div>
       </div>
     );
   }
